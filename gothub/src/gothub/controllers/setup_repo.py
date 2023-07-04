d3651b476d851c90390f013a2ce1981e4081ccba
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 15:40:15 -0700

    so far

--- gothub/src/gothub/controllers/setup_repo.py
+++ gothub/src/gothub/controllers/setup_repo.py
@@ -5,6 +5,8 @@ from typing import Optional
 
 # third-party imports
 from pydantic.dataclasses import dataclass
+from github import Github
+from git import Repo
 
 
 SETUP_BASE_DIR = Path("./repos")
@@ -25,9 +27,18 @@ def mkdir_if_available_else_error(path: Path):
         path.mkdir()
 
 
-def setup_repo(inp: SetupRepoControllerInp):
+def setup_repo(inp: SetupRepoControllerInp) -> Path:
+    dir = SETUP_BASE_DIR / inp.setup_dir
+
     # Make dir in setup_dir
-    mkdir_if_available_else_error(SETUP_BASE_DIR / inp.setup_dir)
+    mkdir_if_available_else_error(dir)
 
     # Clone repo in dir
+    gh = Github(inp.github_token)
+    repo = gh.get_repo(inp.https_url)
+
+    # os.system(f"git clone {inp.https_url} {inp.setup_dir}")
+
     # Checkout branch
+
+    return dir
