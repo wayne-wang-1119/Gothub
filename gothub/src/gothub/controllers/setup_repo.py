aaec98d504c3245d84c1b8ec233bf72f382fec20
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 14:46:33 -0700

    say this

--- gothub/src/gothub/controllers/setup_repo.py
+++ gothub/src/gothub/controllers/setup_repo.py
@@ -1,10 +1,11 @@
+from pathlib import Path
 from typing import Optional
 from pydantic.dataclasses import dataclass
 
 
 @dataclass
 class SetupRepoControllerInp:
-    setup_dir: str
+    setup_dir: Path
     https_url: str
     github_token: str
     branch_name: Optional[str]
