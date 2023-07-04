1fd83300a84c7872520a1000f176d00b4558ec4f
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 16:31:26 -0700

    so far

--- gothub/src/gothub/order.py
+++ gothub/src/gothub/order.py
@@ -1,9 +1,16 @@
 # stdlib imports
+from datetime import datetime
 from typing import Optional
 
 # third-party imports
 from github.PullRequest import PullRequest
 from pydantic.dataclasses import dataclass
+
+# local imports
+from .setup_repo import setup_repo, SetupRepoControllerInp
+
+
+SETUP_ORDERS_BASE_DIR = "orders/"
 
 
 @dataclass
@@ -21,4 +28,15 @@ class GithubOrderOut:
 
 
 def take_order(inp: GithubOrderInp) -> GithubOrderOut:
-    pass
+    time = datetime.now().strftime("%Y-%m-%d %H_%M_%S")
+    dir = SETUP_ORDERS_BASE_DIR + ... + time
+
+    inp = SetupRepoControllerInp(
+        setup_dir=dir,
+        https_url=TEST_HTTPS_URL,
+        github_token=GITHUB_TOKEN,
+        branch_name=OTHER_BRANCH,
+    )
+
+    with setup_repo(inp) as repo:
+        pass
--- gothub/src/gothub/tests/test_setup_repo.py
+++ gothub/src/gothub/tests/test_setup_repo.py
@@ -21,6 +21,7 @@ dotenv.load_dotenv()
 
 
 SETUP_TESTS_BASE_DIR = "tests/"
+
 TEST_HTTPS_URL = "https://github.com/Git-of-Thoughts/GoT-test.git"
 OTHER_BRANCH = "modify-snake.py"
 GITHUB_TOKEN = os.getenv("GITHUB_GOT_TEST_TOKEN")
