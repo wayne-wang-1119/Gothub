761daa6f3dc0995ecbb0d7d390ab256e076e209c
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 16:46:15 -0700

    say this

--- gothub/src/gothub/order.py
+++ gothub/src/gothub/order.py
@@ -15,6 +15,7 @@ SETUP_ORDERS_BASE_DIR = "orders/"
 
 @dataclass
 class GithubOrderInp:
+    username: str
     https_url: str
     github_token: str
     openai_api_key: str
@@ -24,19 +25,31 @@ class GithubOrderInp:
 
 @dataclass
 class GithubOrderOut:
+    order_id: str
     pull_requests: list[PullRequest]
 
 
 def take_order(inp: GithubOrderInp) -> GithubOrderOut:
-    time = datetime.now().strftime("%Y-%m-%d %H_%M_%S_%f")
-    dir = SETUP_ORDERS_BASE_DIR + ... + time
+    match inp:
+        case GithubOrderInp(
+            username=username,
+            https_url=https_url,
+            github_token=github_token,
+            openai_api_key=openai_api_key,
+            branch_name=branch_name,
+            extra_prompt=extra_prompt,
+        ):
+            pass
 
-    inp = SetupRepoControllerInp(
+    time = datetime.now().strftime("%Y-%m-%d %H_%M_%S_%f")
+    dir = SETUP_ORDERS_BASE_DIR + username + "/" + time
+
+    setup_repo_inp = SetupRepoControllerInp(
         setup_dir=dir,
-        https_url=TEST_HTTPS_URL,
-        github_token=GITHUB_TOKEN,
-        branch_name=OTHER_BRANCH,
+        https_url=https_url,
+        github_token=github_token,
+        branch_name=branch_name,
     )
 
-    with setup_repo(inp) as repo:
+    with setup_repo(setup_repo_inp) as repo:
         pass
