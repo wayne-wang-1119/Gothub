914d14caca678bf1af92c33b0f37dfd59b78058f
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 15:05:26 -0700

    how about

--- gothub/src/gothub/controllers/tests/test_setup_repo.py
+++ gothub/src/gothub/controllers/tests/test_setup_repo.py
@@ -15,11 +15,12 @@ SETUP_TESTS_BASE_DIR = "tests"
 
 
 def test_mkdir_if_available_else_error():
-    path = Path(
-        SETUP_BASE_DIR / SETUP_TESTS_BASE_DIR / "test_mkdir_if_available_else_error",
-    )
+    dir = "test_mkdir_if_available_else_error"
+    path = Path(SETUP_BASE_DIR / SETUP_TESTS_BASE_DIR / dir)
     mkdir_if_available_else_error(path)
+
     assert path.exists()
+
     path.rmdir()
 
 
