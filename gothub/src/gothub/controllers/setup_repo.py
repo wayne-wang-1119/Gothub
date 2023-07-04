2462374e28768369e79031f4b2051769adde227d
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 14:51:39 -0700

    so far

--- gothub/src/gothub/controllers/setup_repo.py
+++ gothub/src/gothub/controllers/setup_repo.py
@@ -1,5 +1,9 @@
+# stdlib imports
+import os
 from pathlib import Path
 from typing import Optional
+
+# third-party imports
 from pydantic.dataclasses import dataclass
 
 
@@ -9,3 +13,11 @@ class SetupRepoControllerInp:
     https_url: str
     github_token: str
     branch_name: Optional[str]
+
+
+def setup_repo(inp: SetupRepoControllerInp):
+    # Make dir in setup_dir
+    os.mkdir(inp.setup_dir)
+
+    # Clone repo in dir
+    # Checkout branch
