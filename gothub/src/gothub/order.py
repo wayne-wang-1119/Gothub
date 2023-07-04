37b1e5bec940695f20f55fdcc81dab50d146c390
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 14:35:03 -0700

    so far

--- gothub/src/gothub/order.py
+++ gothub/src/gothub/order.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic.dataclasses import dataclass
 
 
@@ -6,8 +7,14 @@ class GithubPubOrderInp:
     https_url: str
     github_token: str
     openai_api_key: str
+    branch_name: Optional[str]
+    extra_prompt: Optional[str]
 
 
 @dataclass
 class GithubPubOrderOut:
     pull_requests: list[str]
+
+
+def take_order(inp: GithubPubOrderInp) -> GithubPubOrderOut:
+    pass
