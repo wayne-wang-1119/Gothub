ec8fc37c4eb25370a61e9a67fd260e42876d6f65
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 15:43:01 -0700

    how about

--- gothub/src/gothub/controllers/setup_repo.py
+++ gothub/src/gothub/controllers/setup_repo.py
@@ -28,16 +28,25 @@ def mkdir_if_available_else_error(path: Path):
 
 
 def setup_repo(inp: SetupRepoControllerInp) -> Path:
-    dir = SETUP_BASE_DIR / inp.setup_dir
+    match inp:
+        case SetupRepoControllerInp(
+            setup_dir=setup_dir,
+            https_url=https_url,
+            github_token=github_token,
+        ):
+            pass
+
+    dir = SETUP_BASE_DIR / setup_dir
 
     # Make dir in setup_dir
     mkdir_if_available_else_error(dir)
 
     # Clone repo in dir
-    gh = Github(inp.github_token)
-    repo = gh.get_repo(inp.https_url)
+    # gh = Github(github_token)
+    # repo = gh.get_repo(https_url)
+    Repo.clone_from(https_url, dir)
 
-    # os.system(f"git clone {inp.https_url} {inp.setup_dir}")
+    # os.system(f"git clone {https_url} {setup_dir}")
 
     # Checkout branch
 
