b8bbf768ddc45759d7ac3d9041f889da339ce165
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 16:57:25 -0700

    how about

---
+++ gothub/src/gothub/tests/test_order.py
@@ -0,0 +1,29 @@
+from . import (
+    TEST_HTTPS_URL,
+    OTHER_BRANCH,
+    GITHUB_TOKEN,
+)
+from ..order import (
+    take_order,
+    GithubOrderInp,
+    GithubOrderOut,
+)
+
+
+TESTS_USERNAME = "tests"
+
+
+def test_order():
+    inp = GithubOrderInp(
+        username=TESTS_USERNAME,
+        https_url=TEST_HTTPS_URL,
+        github_token=GITHUB_TOKEN,
+        openai_api_key="",
+        branch_name=None,
+        extra_prompt=None,
+    )
+
+    out = take_order(inp)
+
+    assert isinstance(out, GithubOrderOut)
+    assert out.order_id is not None
