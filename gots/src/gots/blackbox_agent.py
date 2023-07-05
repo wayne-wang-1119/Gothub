70c36b8115bf6c798257560b98a0958ea2f646e6
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 22:34:21 -0700

    yeah?

--- gots/src/gots/blackbox_agent.py
+++ gots/src/gots/blackbox_agent.py
@@ -3,50 +3,37 @@ import langchain
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
 from langchain.chat_models import ChatOpenAI
-from .callbacks.CallbackHandler import MyCustomHandlerOne
-from .callbacks.utils.git_methods import git_methods
+from .callbacks.git_callback_handler import GitCallbackHandler
 from .tools.file_tools import (
     read_directory_tree_tool,
     read_one_file_tool,
     write_file_tool,
 )
 
-# keep this true if you want to see the outputs
-langchain.debug = True
 
-cwd = os.getcwd()
+def one_branch_agent(openai_api_key: str):
+    git_callback_handler = GitCallbackHandler()
 
-github_token = os.environ.get("GITHUB_TOKEN")
-openai_api_key = os.environ.get("OPENAI_API_KEY")
+    tools = [
+        read_directory_tree_tool,
+        read_one_file_tool,
+        write_file_tool,
+    ]
 
-repo_url = "https://github.com/Git-of-Thoughts/Gothub.git"
-local_dir = cwd
-access_token = github_token
+    llm = ChatOpenAI(
+        temperature=0,
+        model="gpt-3.5-turbo-0613",
+        callbacks=[
+            git_callback_handler,
+        ],
+        openai_api_key=openai_api_key,
+    )
 
-git = git_methods(repo_url, local_dir, access_token)
-handler = MyCustomHandlerOne(git)
+    mrkl = initialize_agent(
+        tools,
+        llm,
+        agent=AgentType.OPENAI_MULTI_FUNCTIONS,
+        verbose=True,
+    )
 
-llm = ChatOpenAI(
-    temperature=0,
-    model="gpt-3.5-turbo-0613",
-    callbacks=[handler],
-    openai_api_key=openai_api_key,
-)
-
-tools = [
-    read_directory_tree_tool,
-    read_one_file_tool,
-    write_file_tool,
-]
-
-
-mrkl = initialize_agent(
-    tools,
-    llm,
-    agent=AgentType.OPENAI_MULTI_FUNCTIONS,
-    verbose=True,
-)
-
-
-def one_branch_agent():
-    pass
+    mrkl.run("write me a fun python script file")
