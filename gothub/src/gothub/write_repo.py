5f77d905d97061a244e7149818f3bf2bde6f9b24
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 17:42:04 -0700

    let's say

---
+++ gothub/src/gothub/write_repo.py
@@ -0,0 +1,19 @@
+# stdlib imports
+import os
+from pathlib import Path
+from typing import Optional
+
+# third-party imports
+from pydantic.dataclasses import dataclass
+from pydantic import BaseModel
+from github import Github
+from git import Repo
+
+
+class WriteRepoInp(BaseModel):
+    repo: Repo
+    openai_api_key: str
+    extra_prompt: Optional[str]
+
+    class Config:
+        arbitrary_types_allowed = True
