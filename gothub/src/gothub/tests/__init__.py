44bbfae1768eb37d3a62e1a602356dc50e5e1ad5
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 17:04:37 -0700

    works

--- gothub/src/gothub/tests/__init__.py
+++ gothub/src/gothub/tests/__init__.py
@@ -3,4 +3,4 @@ import os
 
 TEST_HTTPS_URL = "https://github.com/Git-of-Thoughts/GoT-test.git"
 OTHER_BRANCH = "modify-snake.py"
-GITHUB_TOKEN = os.getenv("GITHUB_GOT_TEST_TOKEN")
+GITHUB_TOKEN = str(os.getenv("GITHUB_GOT_TEST_TOKEN"))
