c552ac681d97e4ebddad12d76688e779f68da538
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-04 17:50:18 -0700

    ok

--- gothub/src/gothub/write_repo.py
+++ gothub/src/gothub/write_repo.py
@@ -1,12 +1,9 @@
 # stdlib imports
-import os
-from pathlib import Path
-from typing import Optional
+from typing import Optional, Callable
 
 # third-party imports
 from pydantic.dataclasses import dataclass
 from pydantic import BaseModel
-from github import Github
 from git import Repo, Head
 
 
@@ -19,6 +16,11 @@ class WriteRepoInp(BaseModel):
         arbitrary_types_allowed = True
 
 
-@dataclass
-class WriteRepoOut:
+class WriteRepoOut(BaseModel):
     new_branches: list[Head]
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+RepoWriter = Callable[[WriteRepoInp], WriteRepoOut]
