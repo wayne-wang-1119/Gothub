23e68bfe695ef48dede1e824bbf63c7999985566
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-07-06 01:33:36 -0700

    what if

--- gots/src/gots/callbacks/git_callback_handler.py
+++ gots/src/gots/callbacks/git_callback_handler.py
@@ -13,19 +13,17 @@ class GitCallbackHandler(BaseCallbackHandler):
     def on_tool_start(
         self,
         serialized: Dict[str, Any],
-        inputs: str,
+        input_str: str,
         **kwargs: Any,
     ) -> Any:
         """Run when chain starts running."""
-        self.repo.git.add(A=True)
-        self.repo.git.commit(inputs)
 
     def on_tool_end(
         self,
-        outputs: str,
+        output: str,
         **kwargs: Any,
     ) -> Any:
         """Run when chain ends running."""
 
         self.repo.git.add(A=True)  # This will add all files to the staging area
-        self.repo.index.commit("one step done")
+        self.repo.index.commit(output)
