7556cad7453f780f61d45ba6047348ffb1867d90
Haohan Wang <92068838+wayne-wang-1119@users.noreply.github.com>
2023-07-24 18:18:02 -0700

    ok

--- gots/src/gots/tests/test_agent_prompt.py
+++ gots/src/gots/tests/test_agent_prompt.py
@@ -1,15 +1,19 @@
 import os
 
+from dotenv import load_dotenv
 from git import Repo
 
 from gots.repo_agent import WriteRepoInp, WriteRepoOut, one_branch_mrkl
+
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
 
 
 def main():
     # assuming WriteRepoInp is a dataclass or similar object
     repo_inp = WriteRepoInp(
         repo=Repo(str(os.getcwd())),  # assuming Repo is the type of your repo object
-        openai_api_key="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",
+        openai_api_key=openai_api_key,
         extra_prompt="help me write a script that uses some gpt4 apis",
     )
 
