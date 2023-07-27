6d48fd5e4785722c4b6a7c04168a4e4b664ab848
Your Name <you@example.com>
2023-07-27 21:41:49 +0000

    can save a bit

--- gothub/src/gothub/setup_venv.py
+++ gothub/src/gothub/setup_venv.py
@@ -22,8 +22,8 @@ def setup_venv(root_path: Path):
         cmd,
         shell=True,
         executable="/bin/bash",
-        # stdout=subprocess.PIPE,
-        # stderr=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
         text=True,
     )
 
