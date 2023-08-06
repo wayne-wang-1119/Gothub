886974e7a03907ec8a6483e55b8c21bcf626df72
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-08-06 00:14:03 +0000

    works

--- scripts/release_django.sh
+++ scripts/release_django.sh
@@ -1,3 +1,6 @@
 cd gothub_server/src/
-PYTHON="/home/kingh/disk-1/miniconda3/envs/gothub/bin/python"
-$PYTHON manage.py runserver 0.0.0.0:80
+# PYTHON="/home/kingh/disk-1/miniconda3/envs/gothub/bin/python"
+# $PYTHON manage.py runserver 0.0.0.0:80
+# $PYTHON manage.py runserver 0.0.0.0:80 --cert /tmp/cert
+
+python -m gunicorn gothub_server.wsgi:application --bind 127.0.0.1:8000
