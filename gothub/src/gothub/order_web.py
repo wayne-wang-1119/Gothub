6e26f4cc790a3f950b48619b48c859ab975f3231
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-08-08 07:10:10 +0000

    don't do

--- gothub/src/gothub/order_web.py
+++ gothub/src/gothub/order_web.py
@@ -112,11 +112,11 @@ def take_order_web(inp: Order) -> OrderOut:
         for branch in new_branches
     ]
 
-    # !!! Use Sweep
-    sweep_issue = github_repo.create_issue(
-        title=f"Sweep: {name}",
-        body=prompt,
-    )
+    # # !!! Use Sweep
+    # sweep_issue = github_repo.create_issue(
+    #     title=f"Sweep: {name}",
+    #     body=prompt,
+    # )
 
     order_out = OrderOut(
         order=inp,
