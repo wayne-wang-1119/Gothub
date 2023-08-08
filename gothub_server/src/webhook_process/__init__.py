ad6c436a05b4f3deabbe021cebb09e2f908d2edc
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-08-08 07:52:05 +0000

    ok

--- gothub_server/src/webhook_process/__init__.py
+++ gothub_server/src/webhook_process/__init__.py
@@ -1,9 +1,12 @@
 from pathlib import Path
 
 import firebase_admin
-from firebase_admin import auth, credentials
+from firebase_admin import auth, credentials, firestore
 
 DIR_PATH = Path(__file__).absolute().parent.parent.parent.parent
 
 _cred = credentials.Certificate(DIR_PATH / "gothubai-firebase-admin-sdk.json")
 firebase_admin.initialize_app(_cred)
+
+
+firestore_client = firestore.client()
