b8ac85e5ffc7c586c1d3336b91ad4db7a5a02cd5
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-08-08 09:29:53 +0000

    ok

--- gothub/src/gothub/__init__.py
+++ gothub/src/gothub/__init__.py
@@ -1,2 +1,19 @@
+import argparse
+
+
 def main():
+    parser = argparse.ArgumentParser(description="GPT Engineer")
+    parser.add_argument(
+        "prompt",
+        nargs="+",
+        type=str,
+        default="create a web server that greets people in a web page,use python",
+        help="Prompt line",
+    )
+    args = parser.parse_args()
+    line = ""
+    for i in args.prompt:
+        line += i + " "
+    print(f'Requirement: {line.strip("")}')
+
     print("Hello Gothub!")
