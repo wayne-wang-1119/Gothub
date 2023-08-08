f77bbbd4736e695e4c1eb0e3d596e8f63d773bd0
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-08-08 07:15:31 +0000

    harmless

--- gothub/src/gothub/write_github.py
+++ gothub/src/gothub/write_github.py
@@ -13,13 +13,21 @@ class GothubPullRequest(BaseModel):
         arbitrary_types_allowed = True
 
 
-def create_pull_request(github_repo: Repository, branch: Head) -> GothubPullRequest:
+def create_pull_request(
+    github_repo: Repository,
+    branch: Head,
+    title: Optional[str] = None,
+    body: Optional[str] = None,
+) -> GothubPullRequest:
     branch_name = branch.name
     base_branch = github_repo.default_branch
 
+    title = title or f"PR for: {branch_name}"
+    body = body or f"This is a pull request for changes in the branch: {branch_name}"
+
     pr = github_repo.create_pull(
-        title=f"PR for: {branch_name}",
-        body=f"This is a pull request for changes in the branch: {branch_name}",
+        title=title,
+        body=body,
         head=branch_name,  # name of the branch you want to merge
         base=base_branch,
     )
