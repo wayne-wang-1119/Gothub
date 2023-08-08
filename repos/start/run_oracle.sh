f50a6c2c4251556305a29866f29082ade6230256
kingh0730 <46679373+kingh0730@users.noreply.github.com>
2023-08-08 08:45:37 +0000

    intersting

--- repos/start/run_oracle.sh
+++ repos/start/run_oracle.sh
@@ -9,8 +9,8 @@ conda_env_name=".venv_oracle_$1"
 # Activate venv
 source $conda_env_name/bin/activate
 
-# Run
-main
+# Run main with arguments except the first one
+main "${@:2}"
 
 # TODO Support for arguments
 # TODO Support for multiple commands
