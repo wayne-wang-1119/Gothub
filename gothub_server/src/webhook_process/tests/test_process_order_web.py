d5aa135255514ab68b964637c1fcd31e386ee60c
zxy <zxy181101@163.com>
2023-08-25 19:00:03 -0700

    temp

--- gothub_server/src/webhook_process/tests/test_process_order_web.py
+++ gothub_server/src/webhook_process/tests/test_process_order_web.py
@@ -3,10 +3,10 @@ from webhook_process.tasks import process_order_web
 
 def test():
     process_order_web(
-        "",
-        "",
-        "",
-        "",
+        "test_order_id",
+        "test_user_id",
+        "test_username",
+        "https://github.com/Git-of-Thoughts/GoT-test.git",
         "",
         "",
         [],
