30cdde51a4f04271e97a6f23ab21cc905c113924
zxy <zxy181101@163.com>
2023-08-25 19:04:40 -0700

    random id

--- gothub_server/src/webhook_process/tests/test_process_order_web.py
+++ gothub_server/src/webhook_process/tests/test_process_order_web.py
@@ -1,9 +1,13 @@
+from random import random
+
 from webhook_process.tasks import process_order_web
 
 
 def test():
+    random_order_id = str(random())
+
     process_order_web(
-        "test_order_id",  # FIXME Needs to be random every time
+        f"test_order_id_{random_order_id}",  # FIXME Needs to be random every time
         "test_user_id",
         "test_username",
         "https://github.com/Git-of-Thoughts/GoT-test.git",
